ca8eead5e383a4ff7eb51a128fa13883b637129d
Mario Malcek <mmalcek@gmail.com>
2021-06-17 12:07:43 +0200

    build windows, linux

--- build.cmd
+++ build.cmd
@@ -1,2 +1,42 @@
-@echo OFF
-go build -trimpath -ldflags "-s -w" -o ./dist/xmltransform.exe
+@ECHO OFF
+IF [%1]==[all] CALL :ALL
+IF [%1]==[clean] CALL :CLEAN
+IF [%1]==[zip] CALL :ZIP
+IF [%1]==[windows] CALL :WINDOWS
+IF [%1]==[linux] CALL :LINUX
+EXIT /B 1
+
+:ALL
+    CALL :CLEAN
+    CALL :WINDOWS
+    CALL :LINUX
+    CALL :ZIP
+EXIT /B 0
+
+:CLEAN
+    CALL RMDIR "./dist" /q /s
+EXIT /B 0
+
+:WINDOWS
+    SET GOOS=windows
+    SET GOARCH=amd64
+    CALL go build -trimpath -ldflags "-s -w" -o ./dist/windows/xmltransform.exe
+    COPY "inputdata.xml" "./dist/windows/inputdata.xml"
+    COPY "template.tmpl" "./dist/windows/template.tmpl"
+    ECHO xmltransform.exe -i inputdata.xml -o outputFile.csv -t template.tmpl > ./dist/windows/testme.bat
+    ECHO "WINDOWS ready"
+EXIT /B 0
+
+:LINUX
+    SET GOOS=linux
+    SET GOARCH=amd64
+    CALL go build -trimpath -ldflags "-s -w" -o ./dist/linux/xmltransform
+    COPY "inputdata.xml" "./dist/linux/inputdata.xml"
+    COPY "template.tmpl" "./dist/linux/template.tmpl"
+    ECHO "LINUX ready"
+EXIT /B 0
+
+:ZIP
+    CALL 7z.exe a -tzip ./dist/dist.zip ./dist
+    ECHO "ZIP ready"
+EXIT /B 0
